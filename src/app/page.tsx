5fbaf0dce9c4dd1ed718bf87876fdac57aff37ce
AL Fahim <aamfahim@myseneca.ca>
2025-07-12 15:01:49 -0400

    update deprecated zod method

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ const formSchema = z.object({
   firstName: z.string().min(1, "First name is required"),
   lastName: z.string().min(1, "Last name is required"),
   phoneNumber: z.string().min(1, "Phone number is required"),
-  email: z.string().email("Invalid email address").min(1, "Email is required"),
+  email: z.email("Invalid email address").min(1, "Email is required"),
   airFryerCost: z
     .string()
     .min(1, "Air fryer cost guess is required")
